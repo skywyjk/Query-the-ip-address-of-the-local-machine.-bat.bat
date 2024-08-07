1dc1ad2b77780b3289ed1d97129914335e68df39
skywyjk <121789820+skywyjk@users.noreply.github.com>
2024-08-07 20:47:32 +0800

    这四个都能用v0.0.1

---
+++ 查看本机的ip地址3.bat
@@ -0,0 +1,15 @@
+@echo off
+echo.本机电脑IP
+ipconfig|find "IPv4"
+echo.
+echo.本机电脑IP
+ipconfig|find "IPv6"
+echo.
+echo.本机计算机名称
+hostname
+echo.
+echo 网页主页
+echo.www.github.com
+echo.
+pause>nul
+exit
---
+++ 查看本机的ip地址并输出一个ip.txt文件.bat
@@ -0,0 +1,8 @@
+@echo off
+:main
+cls
+ipconfig /all
+@echo
+ipconfig /all>ip.txt
+Echo ip.txt
+@pause
